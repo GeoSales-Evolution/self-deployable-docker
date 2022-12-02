a76be47225cedb31f5d17b8d760b54c8c2f53f2c
Jefferson Quesado <jeff.quesado@gmail.com>
2022-12-02 11:19:37 -0300

    chore(suicide-squad): hello world using PUT and servlet

--- ouroborus/README.md
+++ ouroborus/README.md
@@ -2,3 +2,5 @@
 
 A simple ruby webrick server with an end point that
 restart its container.
+
+`PUT` in `/shutdown` to make this server go down
--- ouroborus/lib/ouroborus.rb
+++ ouroborus/lib/ouroborus.rb
@@ -2,12 +2,36 @@
 
 require 'webrick'
 
+class ShutDownServlet < WEBrick::HTTPServlet::AbstractServlet
+  def do_PUT request, response
+    response.status = 200
+    response.content_type = "text/plain"
+    response.body = 'Hello, World!'
+  end
+end
+
 def ouroborus_server(port = 8000)
   server = WEBrick::HTTPServer.new :Port => port
 
   server.mount_proc '/' do |req, res|
+    if req.path != '/' then
+      res.status = 404
+      res.content_type = "text/plain"
+      res.body = 'oops'
+      next
+    end
+    if req.request_method != 'GET' then
+      res.status = 405
+      res.content_type = "text/plain"
+      res.body = "oops, use GET instead of #{req.request_method}"
+      next
+    end
+    puts "req #{req.path}"
     res.body = 'Hello, world'
   end
+  puts 'montar servlet'
+  server.mount '/shutdown', ShutDownServlet
+  puts 'montou servlet'
 
   trap 'INT' do server.shutdown end
 
