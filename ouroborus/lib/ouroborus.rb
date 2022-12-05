e953802f95e1a8676fa761c8d51a9751ab56f191
Jefferson Quesado <jeff.quesado@gmail.com>
2022-12-05 03:26:46 -0300

    feat(ouroborus): respawn complete

--- ouroborus/lib/ouroborus.rb
+++ ouroborus/lib/ouroborus.rb
@@ -16,6 +16,28 @@ class ShutDownServlet < WEBrick::HTTPServlet::AbstractServlet
   end
 end
 
+class RespawnServlet < WEBrick::HTTPServlet::AbstractServlet
+  def initialize server, suicide_squad, port
+    super server
+    @suicide_squad = suicide_squad
+    @port = port
+  end
+  def do_PUT request, response
+    response.status = 200
+    response.content_type = "text/plain"
+    response.body = hestia
+
+    @suicide_squad.call
+  end
+
+  def hestia
+    docker_socket = "/var/run/docker.sock"
+    docker_socket_bind = "-v #{docker_socket}:#{docker_socket}"
+    port_bind = "-p #{@port}:#{@port}"
+    `docker run -d #{docker_socket_bind} hestia -- run -d --restart unless-stopped #{port_bind} #{docker_socket_bind} ouroborus:latest`
+  end
+end
+
 def ouroborus_server(port = 8000)
   server = WEBrick::HTTPServer.new :Port => port
 
@@ -43,10 +65,13 @@ def ouroborus_server(port = 8000)
     end
   }
   server.mount '/shutdown', ShutDownServlet, suicide_squad
+  server.mount '/respawn', RespawnServlet, suicide_squad, port
   puts 'montou servlet'
 
   trap 'INT' do server.shutdown end
 
   puts 'iniciando o server'
   server.start
+  hostname=`hostname`
+  puts `docker stop #{hostname}`
 end
