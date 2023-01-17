3c04c4ebc730255a2f0d742586495c8253792a11
Jefferson Quesado <jeff.quesado@gmail.com>
2023-01-16 23:32:54 -0300

    Adding port to container

--- ouroborus/test/test_container.rb
+++ ouroborus/test/test_container.rb
@@ -12,3 +12,15 @@ describe Ouroborus::Args do
     expect(args.to_s).to eq('abc lalala\\ lelele')
   end
 end
+describe Ouroborus::Container do
+  it 'should add an arg to a container' do
+    container = Ouroborus::Container.new name: 'lala', image: 'lala'
+    container.port 27
+    expect(container.args.to_s).to eq("-p 27")
+  end
+  it 'should add an arg to a container' do
+    container = Ouroborus::Container.new name: 'lala', image: 'lala'
+    container.port 80,8080
+    expect(container.args.to_s).to eq("-p 80:8080")
+  end
+end
